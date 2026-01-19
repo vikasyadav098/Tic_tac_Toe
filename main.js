897197cce60328874fd22d22651d11a641d4eed0
vikasyadav098 <vikyadav098@gmail.com>
2026-01-19 10:41:08 +0530

    Javascript Code

---
+++ main.js
@@ -0,0 +1,72 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newGamebtn = document.querySelector("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+const resetgame = () => {
+  turnO = true;
+  enableBoxes();
+  msgcontainer.classList.add("hide");
+  msg.innerText="";
+}
+ 
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+     
+  }
+};
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations,Winner is${winner}`;
+  msgcontainer.classList.remove("hide");
+  disableBoxes();
+};
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        
+
+        showWinner(pos1Val);
+      }
+    }
+  }
+};
+newGamebtn.addEventListener("click", resetgame);
+resetbtn.addEventListener("click", resetgame);
